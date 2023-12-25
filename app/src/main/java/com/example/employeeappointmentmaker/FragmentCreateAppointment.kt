8f3423b8fea2f018fa77e0dde67a0df0a5198f7e
priyank1968 <priyank1968@gmail.com>
2023-12-25 16:55:43 -0500

    Locale Issue

---
+++ app/src/main/java/com/example/employeeappointmentmaker/FragmentCreateAppointment.kt
@@ -0,0 +1,7 @@
+package com.example.employeeappointmentmaker
+
+import androidx.fragment.app.Fragment
+
+class FragmentCreateAppointment : Fragment(R.layout.fragment_create_appointment) {
+    //TODO Fields that will take information of the customer and the appointments they are making
+}
--- app/src/main/java/com/example/employeeappointmentmaker/MainActivity.kt
+++ app/src/main/java/com/example/employeeappointmentmaker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.employeeappointmentmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isEmpty
+import com.example.employeeappointmentmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentList = FragmentAppointmentListView()
+        val fragmentCreateAppointment = FragmentCreateAppointment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentLayout, fragmentList)
+            commit()
+        }
+
+        binding.buttonCreate.setOnClickListener {
+            Log.e("test", "fragmentCreateAppointment click")
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentLayout, fragmentCreateAppointment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 }
